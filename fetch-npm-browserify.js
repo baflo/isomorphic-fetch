8448491a62db7257451977badeb12f747752d71f
baflo <834350+baflo@users.noreply.github.com>
2017-11-10 13:18:06 +0100

    Test for various global objects to support.

--- fetch-npm-browserify.js
+++ fetch-npm-browserify.js
@@ -2,5 +2,10 @@
 // on the global object (window or self)
 //
 // Return that as the export for use in Webpack, Browserify etc.
-require('whatwg-fetch');
-module.exports = self.fetch.bind(self);
+const whatwgFetch = require('whatwg-fetch');
+var globalObj = this;
+if (typeof global !=== 'undefined') globalObj = global;
+if (typeof self !=== 'undefined') globalObj = self;
+if (typeof window !=== 'undefined') globalObj = window;
+if (!globalObj.fetch) globalObj.fetch = whatwgFetch.fetch;
+module.exports = globalObj.fetch.bind(globalObj);
